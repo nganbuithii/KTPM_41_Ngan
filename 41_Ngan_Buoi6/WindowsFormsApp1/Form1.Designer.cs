6aa641e9c9e305e039cb1fed12753abe6992d99f
Ngân <nganbui.23112003@gmail.com>
2024-04-06 01:14:00 +0700

    Selenium p2 - buổi 6

---
+++ 41_Ngan_Buoi6/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenUrl = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(179, 110);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(469, 41);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "url";
+            // 
+            // btnOpenUrl
+            // 
+            this.btnOpenUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenUrl.Location = new System.Drawing.Point(211, 279);
+            this.btnOpenUrl.Name = "btnOpenUrl";
+            this.btnOpenUrl.Size = new System.Drawing.Size(192, 50);
+            this.btnOpenUrl.TabIndex = 2;
+            this.btnOpenUrl.Text = "Open url";
+            this.btnOpenUrl.UseVisualStyleBackColor = true;
+            this.btnOpenUrl.Click += new System.EventHandler(this.btnOpenUrl_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 36);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tiêu đề";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(179, 49);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(459, 41);
+            this.txtTitle.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 36);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "inputText";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(179, 180);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(469, 41);
+            this.txtInput.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 332);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOpenUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Bài 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenUrl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ 41_Ngan_Buoi6/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenUrl_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            if (string.IsNullOrEmpty(url))
+                url = "https://www.google.com.vn/?hl=vi";
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+
+            //driver.Close();
+
+            // tắt màn hình đen
+            //ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            //chrome.HideCommandPromptWindow = true;
+
+            //// Điều hướng trình duyệt
+            //IWebDriver drive = new ChromeDriver(chrome);
+
+            //drive.Quit();
+
+            // lấy url
+            String link_hientai = driver.Url;
+            Console.WriteLine(link_hientai);
+
+            // lấy title
+            String urlTitle = driver.Title;
+            Console.WriteLine(urlTitle);
+
+            //G
+            // Lấy URL hiện tại và tiêu đề của trang
+            string currentUrl = driver.Url;
+            string pageTitle = driver.Title;
+
+            // Hiển thị URL và tiêu đề lên các TextBox tương ứng
+            txtUrl.Text = currentUrl;
+            txtTitle.Text = pageTitle;
+
+
+            //H
+            // LẤY PAGE SOURCE
+            String PageSource = driver.PageSource;
+            Console.WriteLine(PageSource);
+
+            // back
+            //driver.Navigate().Back();
+            //Thread.Sleep(1000);
+            //driver.Navigate().Refresh();
+
+
+            // Tự động điền vào ô tìm kiếm
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("hello world");
+
+
+
+            // Lấy giá trị của phần tử và gán vào TextBox
+            string elementValue = element.GetAttribute("value");
+            txtInput.Text = elementValue;
+
+            driver.Quit();
+
+
+        }
+    }
+}
